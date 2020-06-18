2cdb00632b00d02cf38a7d7c66d83f9eeef48975
lenya1976 <bargamon76@gmail.comgit config --global user.name Your>
2020-06-18 10:55:10 +0300

    goit-react-hw-03-phonebook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import ContactFormManager from "../contactFormManager";
 import styles from "./App.module.css";
 
-export default class App extends Component {
-  state = {
-    contacts: [],
-  };
-  render() {
-    return (
-      <div className={styles.container}>
-        <h1>goit-react-hw-02-phonebook</h1>
-        <ContactFormManager />
-      </div>
-    );
-  }
-}
+export default (App) => {
+  return (
+    <div className={styles.container}>
+      <h1>goit-react-hw-02-phonebook</h1>
+      <ContactFormManager />
+    </div>
+  );
+};
